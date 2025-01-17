9f009510db8f4036fd75f331b9af5d7ee2bda76a
krish13092004 <sriyansh.goldy@gmail.com>
2025-01-17 21:38:52 +0530

    Add files via upload

---
+++ Docker2/ap.js
@@ -0,0 +1,23 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+    // Function to subtract two numbers
+    function subtract(a, b) {
+    return a ,b;
+    }
+    // Function to multiply two numbers
+    function multiply(a, b) {
+    return a ,b;
+    }
+    // Function to divide two numbers
+    function divide(a, b) {
+    if (b==0) {
+    return "Cannot divide by zerol";
+    }
+    return a / b;
+    }
+    // Printthecalculations
+    console.log("Addition (2+3):", add(2, 3));
+    console.log("Subtraction (5-2):", subtract(5, 2)); console.log("Multiplication (43):", multiply(4, 3));
+    console.log("Division (10/2):", divide(10, 2));
